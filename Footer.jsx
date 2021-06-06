330aff36609cdca9052327cdcbceaa358486ce3b
maira-90 <64853743+maira-90@users.noreply.github.com>
2021-06-07 00:03:06 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  var curryear = new Date().getFullYear;
+
+  return (
+    <footer>
+      <p>Copyright @ 2021</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Minimalist Aesthetic:</h1>
+      <p>
+      As much as I like and can identify 
+      with (most parts of) minimalism,
+     I’m equally drawn to the aesthetic side of it. These two things are not necessarily interrelated, though people often assume they must go hand in hand
+      </p>
+    </div>
+  );
+}
+
+export default Note;
